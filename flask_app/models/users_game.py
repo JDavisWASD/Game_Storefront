d137fdb0dacde715c2e5f2970862369d5e44f25a
JDavisWASD <jdavisWASD@gmail.com>
2021-12-18 18:01:19 -0800

    first pass at users_game model

---
+++ flask_app/models/users_game.py
@@ -0,0 +1,65 @@
+from flask_app.config.mysqlconnection import connectToMySQL
+from flask_app.models import game
+
+class UsersGame:
+    DATABASE = 'game_platform'
+
+    def __init__(self, data):
+        self.id = data['id']
+        self.user_id = data['user_id']
+        self.game_id = data['game_id']
+        self.status = data['status']    #'owned' or 'wishlist' only
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+
+#Get functions -----------------------------------------------------------------
+    @classmethod
+    def get_users_games(cls, data):
+        query = 'SELECT * FROM users_games JOIN games on game_id = games.id ' \
+            'WHERE user_id = %(user_id)s AND status = %(status)s;'
+        results = connectToMySQL(cls.DATABASE).query_db(query, data)
+        games = []
+        for row in results:
+            game_data = {
+                'id': row['games.id'],
+                'name': row['name'],
+                'genre': row['genre'],
+                'release_date': row['release_date'],
+                'price': row['price'],
+                'description': row['decription'],
+                'created_at': row['games.created_at'],
+                'updated_at': row['games.updated_at']
+            }
+            games.append(game.Game(game_data))
+
+        return games
+
+#Modify functions --------------------------------------------------------------
+    @classmethod
+    def save(cls, data):
+        if data['status'].lower() != 'owned' and \
+                data['status'].lower() != 'wishlist':
+            print('Error: Status must be "owned" or "wishlist".')
+            return False
+
+        query = 'INSERT INTO users_games (user_id, game_id, status, ' \
+            'created_at, updated_at) VALUES (%(user_id)s, %(game_id)s, ' \
+            '%(status)s, NOW(), NOW());'
+        return connectToMySQL(cls.DATABASE).query_db(query, data)
+
+    @classmethod
+    def update(cls, data):
+        if data['status'].lower() != 'owned' and \
+                data['status'].lower() != 'wishlist':
+            print('Error: Status must be "owned" or "wishlist".')
+            return False
+
+        query = 'UPDATE users_games SET status = %(status)s WHERE ' \
+            'user_id = %(user_id)s AND game_id = %(game_id)s;'
+        connectToMySQL(cls.DATABASE).query_db(query, data)
+
+    @classmethod
+    def delete(cls, data):
+        query = 'DELETE FROM users_games WHERE user_id = %(user_id)s AND ' \
+            'game_id = %(game_id)s;'
+        connectToMySQL(cls.DATABASE).query_db(query, data)
