10ef9ae1856ce90a7362ca706c4dc371bd0a9430
Heather <situh@hawaii.edu>
2021-12-19 20:50:49 -1000

    Renamed variable for logged-in user to 'self_user'

--- flask_app/controllers/users.py
+++ flask_app/controllers/users.py
@@ -33,7 +33,7 @@ def dashboard():
         # display all friends - via 'all_friends' [HS]
         # can remove others from user's friend list - via 'remove_friend' route [HS]
 
-    return render_template('dashboard.html', all_users = user.User.get_all(), all_friends = friend.Friend.get_all_by_user(user_data), wishlist_games = users_game.UsersGame.get_users_games_by_status(wishlist_data), collection_games = users_game.UsersGame.get_users_games_by_status(collection_data))
+    return render_template('dashboard.html', self_user = user.User.get_by_id(user_data), all_games = game.Game.get_all(), all_users = user.User.get_all(), all_friends = friend.Friend.get_all_by_user(user_data), wishlist_games = users_game.UsersGame.get_users_games_by_status(wishlist_data), collection_games = users_game.UsersGame.get_users_games_by_status(collection_data))
 
 # --- Processes user's request to add another user to friends list ---
 @app.route('/add/friend/<int:friend_id>')
@@ -84,3 +84,5 @@ def remove_from_game_category(status, game_id):
     users_game.UsersGame.delete(data)
 
     return redirect('/dashboard')
+
+# -- Log out [JD] --
