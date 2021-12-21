b7ec376d663c7f810058c27f9f9ff1b0e45e5d80
JDavisWASD <jdavisWASD@gmail.com>
2021-12-20 21:10:13 -0800

    /view/game_id to /view/game/game_id

--- flask_app/controllers/games.py
+++ flask_app/controllers/games.py
@@ -2,7 +2,7 @@ from flask import redirect, render_template, session
 from flask_app import app
 from flask_app.models import friend, game, users_game
 
-@app.route('/view/<int:game_id>')
+@app.route('/view/game/<int:game_id>')
 def view_game(game_id):
     if 'user_id' not in session:
         return redirect('/login')
